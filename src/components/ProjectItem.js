7491c865a55a14be2608c96f06b8bfd4d2e15b39
hassan kazungu <hassankazungu01@gmail.com>
2023-01-25 13:03:13 +0300

    done

---
+++ src/components/ProjectItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ProjectItem({ name, about, technologies }) {
+  return (
+    <div className="project-item">
+      <h3>{name}</h3>
+      <p>{about}</p>
+      <div className="technologies">
+        {technologies.map(tech=><span key={tech}>{tech}</span>)}
+      </div>
+    </div>
+  );
+}
+
+export default ProjectItem;
